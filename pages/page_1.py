5da20ee9d6912071314333a75b09562461b818a2
pippayyy <80529128+pippayyy@users.noreply.github.com>
2024-06-03 10:02:10 +0000

    Latest UI changes

--- pages/page_1.py
+++ pages/page_1.py
@@ -4,39 +4,12 @@ import pandas as pd
 import streamlit as st
 import sidebar
 
-st.markdown(
-    """
-    <style>
-        [data-testid=stSidebar] [data-testid=stImage]{
-            text-align: center;
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            width: 100%;
-        }
-        .sidebar .sidebar-content {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .my-text {
-        color: red;
-        }
-
-    </style>
-    """, unsafe_allow_html=True
-)
 
 # Center-aligned header
-st.markdown("<h1 style='text-align: center;'>Spirit AI Tool</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Identify Comment Themes</h1>", unsafe_allow_html=True)
 
 # Center-aligned paragraph
-st.markdown("<p style='text-align: center;'>This tool uses AI to analyse spirit comments quickly, making it easier for Spirit Ambassadors to provide a high-level view of comments.</p>", unsafe_allow_html=True)
-
-# Left-aligned header
-st.markdown("<h1 style='text-align: left;'>How it works?</h1>", unsafe_allow_html=True)
-
+st.markdown("<p style='text-align: center;'>Upload your Spirit Beat comment extract (excel file) to perform a thematic analysis.</p>", unsafe_allow_html=True)
 
 st.markdown(
     """
@@ -58,6 +31,28 @@ st.markdown(
     """, unsafe_allow_html=True
 )
 
+
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+        [data-testid=stSidebar] [data-testid=stImage]{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+        }
+        .sidebar .sidebar-content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+    </style>
+    """, unsafe_allow_html=True
+)
 
 with st.sidebar:
     # VF Icon
@@ -75,29 +70,3 @@ with st.sidebar:
     st.page_link("pages/page_2.py", label="Sentiment", disabled=True)
     st.page_link("http://www.google.com", label="Chat", disabled=True)
 
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
---
+++ sidebar.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+        [data-testid=stSidebar] [data-testid=stImage]{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+        }
+        .sidebar .sidebar-content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+    </style>
+    """, unsafe_allow_html=True
+)
+
+with st.sidebar:
+    # VF Icon
+    st.image('VF_Icon_RGB_RED.png', width=100)
+
+    # Center-aligned header
+    st.markdown("<h3 style='text-align: center;'>Spirit Survey AI Analyser</h3>", unsafe_allow_html=True)
+
+    # Divider
+    st.divider()
+
+    # Nav page links
+    st.page_link("streamlit_app.py", label="Home")
+    st.page_link("pages/page_1.py", label="Themes")
+    st.page_link("pages/page_2.py", label="Sentiment", disabled=True)
+    st.page_link("http://www.google.com", label="Chat", disabled=True)
